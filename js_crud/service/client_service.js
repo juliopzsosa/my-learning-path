8a8db2804aa936027d35ce490a7d197ad25264b6
Julio P <julio.pzsosa@gmail.com>
2023-07-13 00:45:24 -0600

    Class 04 completed for the course 'JS on the Web: CRUD with Asynchronous JavaScript'.

--- js_crud/service/client_service.js
+++ js_crud/service/client_service.js
@@ -9,23 +9,36 @@ const crearCliente = (nombre, email) => {
         },
         body: JSON.stringify({ nombre, email, id: uuid.v4() }),
     });
-}
+};
 
 const eliminarCliente = (id) => {
     return fetch(`https://64ae0f57b470006a5ec692e4.mockapi.io/perfil/${id}`, {
         method: 'DELETE',
     });
-}
+};
+
+const detalleCliente = (id) => {
+    return fetch(`https://64ae0f57b470006a5ec692e4.mockapi.io/perfil/${id}`).then((respuesta) =>
+        respuesta.json()
+    );
+};
+
+const actualizarCliente = (nombre, email, id) => {
+    return fetch(`https://64ae0f57b470006a5ec692e4.mockapi.io/perfil/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ nombre, email }),
+    })
+    .then(respuesta => respuesta)
+    .catch((err) => console.log(err));
+};
 
 export const clientServices = {
     listaClientes,
     crearCliente,
     eliminarCliente,
+    detalleCliente,
+    actualizarCliente,
 };
-
-
-
-
-
-
-
