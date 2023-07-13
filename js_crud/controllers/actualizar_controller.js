c58e129e6c1ccb12904f416c3fc46193b29e9b12
Julio P <julio.pzsosa@gmail.com>
2023-07-13 01:38:07 -0600

    Course 'JS on the Web: CRUD with Asynchronous JavaScript': Completed.

--- js_crud/controllers/actualizar_controller.js
+++ js_crud/controllers/actualizar_controller.js
@@ -2,21 +2,25 @@ import { clientServices } from "../service/client_service.js";
 
 const formulario = document.querySelector("[data-form]");
 
-const obtenerInformacion = () => {
+const obtenerInformacion = async () => {
     const url = new URL(window.location);
     const id = url.searchParams.get("id");
-
     if (id === null) {
         window.location.href = "../screens/error.html";
     };
-
     const nombre = document.querySelector("[data-nombre]");
     const email = document.querySelector("[data-email]");
-
-    clientServices.detalleCliente(id).then((perfil) => {
-        nombre.value = perfil.nombre;
-        email.value = perfil.email;
-    });
+    try {
+        const perfil = await clientServices.detalleCliente(id);
+        if (perfil.nombre && perfil.email) {
+            nombre.value = perfil.nombre;
+            email.value = perfil.email;
+        } else {
+            throw new Error();
+        }
+    } catch (error) {
+        window.location.href = "../screens/error.html"
+    };
 };
 
 obtenerInformacion();
@@ -28,7 +32,7 @@ formulario.addEventListener('submit', (evento) => {
 
     const nombre = document.querySelector("[data-nombre]").value;
     const email = document.querySelector("[data-email]").value;
-    clientServices.actualizarCliente(nombre,email,id).then(() => {
+    clientServices.actualizarCliente(nombre, email, id).then(() => {
         window.location.href = "../screens/edicion_concluida.html";
     });
 });
