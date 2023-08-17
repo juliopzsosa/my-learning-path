cd59e24dd70aeb46c8025c999c60917c8413083a
Julio P <julio.pzsosa@gmail.com>
2023-08-17 17:47:12 -0600

    Class 02 completed for the course 'React Router: SPA Navigation'

--- react-02/src/App.js
+++ react-02/src/App.js
@@ -3,13 +3,17 @@ import "./assets/css/base/base.css";
 import "./assets/css/componentes/card.css";
 import Home from "./pages/Home";
 import Sobre from "./pages/Sobre";
+import Page404 from "./pages/Page404";
+import Header from "./components/Header";
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sobre" element={<Sobre />} />
+        <Route path="*" element={<Page404 />} />
       </Routes>
     </Router>
   );
