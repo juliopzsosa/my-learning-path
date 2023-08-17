283019dbd3f665c9b3437827bfd7ea8e9fec12a3
Julio P <julio.pzsosa@gmail.com>
2023-08-17 17:08:05 -0600

    Class 01 completed for the course 'React Router: SPA Navigation'

---
+++ react-02/src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./assets/css/base/base.css";
+import "./assets/css/componentes/card.css";
+import Home from "./pages/Home";
+import Sobre from "./pages/Sobre";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sobre" element={<Sobre />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
